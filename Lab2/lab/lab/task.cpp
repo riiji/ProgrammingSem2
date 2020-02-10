9619cb6088f9368a1188c36d276f6cdae2c5ccf7
Riijii <riijii333@gmail.com>
2020-02-11 00:47:47 +0300

    lab2 added

---
+++ Lab2/lab/lab/task.cpp
@@ -0,0 +1,55 @@
+﻿#include "pch.h"
+#include "task.h"
+
+complex::complex()
+{
+	this->im = 0;
+	this->re = 0;
+}
+
+complex::complex(const double re, const double im)
+{
+	this->re = re;
+	this->im = im;
+}
+
+complex::complex(const complex& a)
+{
+	this->re = a.re;
+	this->im = a.im;
+}
+
+std::string complex::to_string() const
+{
+	return std::to_string(this->re) + std::to_string(this->im) + "i";
+}
+
+complex complex::operator+(const complex& left) const
+{
+	return complex(this->re + left.re, this->im + left.im);
+}
+
+complex complex::operator+(const double& left) const
+{
+	return complex(this->re + left, this->im);
+}
+
+complex complex::operator*(const complex& left) const
+{
+	return complex(this->re*left.re-this->im*left.im, this->im*left.re + this->re*left.im);
+}
+
+complex complex::operator*(const double left) const
+{
+	return complex(this->re * left, this->im);
+}
+
+bool complex::operator==(const complex& left) const
+{
+	return this->re == left.re && this->im == left.im;
+}
+
+int complex::length() const
+{
+	return this->to_string().length();
+}
---
+++ Lab2/lab/lab/task.h
@@ -0,0 +1,30 @@
+﻿#pragma once
+#include <string>
+
+
+class complex
+{
+public:
+	double re, im;
+
+	complex();
+
+	complex(double, double);
+
+	complex(const complex&);
+
+	std::string to_string() const;
+
+	complex operator+(const complex&) const;
+
+	complex operator+(const double&) const;
+
+	complex operator*(const complex&) const;
+
+	complex operator*(double) const;
+
+	bool operator==(const complex&) const;
+
+	int length() const;
+	
+};
