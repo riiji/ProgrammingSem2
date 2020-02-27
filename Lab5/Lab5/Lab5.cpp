548ed5967a8a0b734bf85d14c01a643428742b1d
Riijii <riijii333@gmail.com>
2020-02-27 18:12:43 +0300

    Lab5 added

---
+++ Lab5/Lab5/Lab5.cpp
@@ -0,0 +1,11 @@
+﻿// Lab5.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include "methods.h"
+
+int main()
+{
+    Array<3, int> a;
+	cout << a[-1];
+}
---
+++ Lab5/Lab5/methods.h
@@ -0,0 +1,42 @@
+﻿#pragma once
+
+using namespace std;
+
+class IndexOutOfRangeException : public exception {
+	const char* what() const throw () {
+		return "Index was out of range";
+	}
+};
+
+static class methods
+{
+public:
+	template<class T>
+	static T GetMin(T a, T b);
+};
+
+template <int N, class T>
+class Array
+{
+	T arr[N];
+
+public:
+	T& operator[](int n);
+};
+
+template <class T>
+T methods::GetMin(T a, T b)
+{
+	return (a < b) ? a : b;
+}
+
+template <int N, class T>
+T& Array<N, T>::operator[](int n)
+{
+	if (n<0 || n>N)
+		throw IndexOutOfRangeException();
+
+	return this->arr[n];
+}
+
+
